17dfb6ac45b5df85f469a40679b394c81dfbda0d
Weerachai <s6102041520165@email.kmutnb.ac.th>
2020-04-02 14:01:47 +0700

    added

--- views/lease/view.php
+++ views/lease/view.php
@@ -1,5 +1,10 @@
 <?php
 
+use app\models\Lease;
+use app\models\LeaseDetail;
+use app\models\OrderDetail;
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
@@ -16,8 +21,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+        <?= Html::a(Yii::t('app', 'แก้ไข'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('app', 'สัญญาเช่า'), ['update', 'id' => $model->id], ['class' => 'btn btn-warning']) ?>
+        <?= Html::a(Yii::t('app', 'ลบ'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => Yii::t('app', 'คุณต้องการลบรายการนี้ใช่หรือไม่'),
@@ -32,11 +38,13 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'lease_date:date',
             'due_date:date',
+            'lease_time:time',
+            'due_time:time',
             'description:ntext',
             [
                 'attribute' => 'grand_total',
                 'value' => function ($data) {
-                    return number_format($data->grand_total);
+                    return number_format($data->grand_total) . " บาท/1 วัน";
                 }
             ],
             'created_at:relativeTime',
@@ -49,5 +57,50 @@ $this->params['breadcrumbs'][] = $this->title;
             'updated_at:relativeTime',
         ],
     ]) ?>
+
+    <?php
+    $dataProvider = new ActiveDataProvider([
+        'query' => LeaseDetail::find()->where(['lease_id' => $model->id]),
+    ]);
+
+    $total = Lease::findOne(['id' => $model->id]);
+    echo "<h1>รายการสินค้าที่สั่งซื้อ</h1>";
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'showFooter' => TRUE,
+        'footerRowOptions' => ['style' => 'font-weight:bold;text-align:right'],
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            [
+                'attribute' => 'product_id',
+                'value' => function ($data) {
+                    return $data->product['name'];
+                }
+            ],
+            [
+                'attribute' => 'product.price_for_order',
+                'value' => function ($data) {
+                    return number_format($data->product['price_for_order']) . " x " . $data->qty;
+                },
+            ],
+            [
+                //'attribute' => 'product.price_for_order',
+                'label' => 'ราคารวม',
+                'contentOptions' => ['style' => 'width: 10%;text-align:right'],
+                'value' => function ($data) {
+
+                    // show the amount in money format => 50,000.00
+                    return number_format($data->product['price_for_order'] * $data->qty, 2);
+                },
+                'filter' => false, //disable the filter for this field
+                // I create the summary function in my Invoice model
+                'footer' => number_format($total->grand_total, 2),
+            ],
+        ],
+        /**/
+
+    ]);
+
+    ?>
 
 </div>
